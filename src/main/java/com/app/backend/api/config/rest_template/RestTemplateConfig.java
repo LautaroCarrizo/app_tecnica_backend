ca0a35ae89609bfbb9a461c1bb5130d49e982e9d
Lautaro Carrizo <laucarrizo332@gmail.com>
2025-08-08 12:31:50 -0300

    primer commit

---
+++ src/main/java/com/app/backend/api/config/rest_template/RestTemplateConfig.java
@@ -0,0 +1,4 @@
+package com.app.backend.api.config.rest_template;
+
+public class RestTemplateConfig {
+}
---
+++ src/main/java/com/app/backend/api/config/spring_security/SpringSecurityClass.java
@@ -0,0 +1,4 @@
+package com.app.backend.api.config.spring_security;
+
+public class SpringSecurityClass {
+}
